1258cf8b0f09c05ead2293383c06b059f25eaf08
Abduallh Shadow <abduallhshadow@gmail.com>
2025-09-17 22:33:57 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/layout/snowfall.tsx
+++ src/components/layout/snowfall.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './snowfall.css';
 
 interface SnowfallProps {
@@ -7,19 +7,28 @@ interface SnowfallProps {
 }
 
 const Snowfall = ({ className }: SnowfallProps) => {
-  const flakes = Array.from({ length: 100 }).map((_, i) => {
-    const style: React.CSSProperties = {
-      '--random-x-start': Math.random(),
-      '--random-x-end': Math.random(),
-      '--random-y-end': Math.random(),
-      '--random-scale': Math.random(),
-      '--random-delay': `${Math.random() * -20}s`,
-      '--random-duration': `${10 + Math.random() * 10}s`,
-      '--random-opacity': Math.random() * 0.5 + 0.5,
-    } as React.CSSProperties;
+  const [flakes, setFlakes] = useState<React.ReactNode[]>([]);
 
-    return <div className="snowflake" key={i} style={style} />;
-  });
+  useEffect(() => {
+    const generateFlakes = () => {
+      return Array.from({ length: 100 }).map((_, i) => {
+        const style: React.CSSProperties = {
+          '--random-x-start': Math.random(),
+          '--random-x-end': Math.random(),
+          '--random-y-end': Math.random(),
+          '--random-scale': Math.random(),
+          '--random-delay': `${Math.random() * -20}s`,
+          '--random-duration': `${10 + Math.random() * 10}s`,
+          '--random-opacity': Math.random() * 0.5 + 0.5,
+        } as React.CSSProperties;
+    
+        return <div className="snowflake" key={i} style={style} />;
+      });
+    };
+    
+    setFlakes(generateFlakes());
+  }, []);
+
   return <div className={`snowfall-container ${className}`}>{flakes}</div>;
 };
 
