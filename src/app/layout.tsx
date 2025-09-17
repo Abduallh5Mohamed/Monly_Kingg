8a2ffea8d049e82c4c505b05b7263c2433785935
Abduallh Shadow <abduallhshadow@gmail.com>
2025-09-17 13:38:50 +0000

    اسمه Monly King

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Rank Ascent',
+  title: 'Monly King',
   description: 'Your Gateway to Premium Game Accounts & Unmatched Powers',
 };
 
