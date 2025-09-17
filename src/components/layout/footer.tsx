413934b847cf2483f9e8a1136369cf5e007669ed
Abduallh Shadow <abduallhshadow@gmail.com>
2025-09-17 22:09:45 +0000

    https://upload.wikimedia.org/wikipedia/commons/2/20/InstaPay_Logo.png?20

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
   const paymentMethods = [
     { name: 'Vodafone Cash', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Vodafone_icon.svg/200px-Vodafone_icon.svg.png' },
     { name: 'PayPal', logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Paypal_2014_logo.png' },
-    { name: 'InstaPay', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/InstaPay_logo.svg/2560px-InstaPay_logo.svg.png' },
+    { name: 'InstaPay', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/20/InstaPay_Logo.png?20230411102327' },
   ];
 
   return (
