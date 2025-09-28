705ac84e4c266405931101adbc7182555183b39f
Abduallh Shadow <abduallhshadow@gmail.com>
2025-09-28 12:25:04 +0000

    نزل صورة فالورانت تحت شويه وزود ال سمك شويه بتاع الاطارات وقلل حده الاطر

--- src/components/account-carousel.tsx
+++ src/components/account-carousel.tsx
@@ -38,12 +38,12 @@ export function AccountCarousel() {
         <CarouselContent className="-ml-4">
           {trendingGames.map((game, index) => (
             <CarouselItem key={index} className="pl-8 md:basis-1/2 lg:basis-1/3">
-              <div className="group relative aspect-[3/4] w-full rounded-lg holographic-border overflow-hidden">
+              <div className="group relative aspect-[3/4] w-full rounded-xl holographic-border overflow-hidden">
                 <Image
                   src={game.logo}
                   alt={'Trending Game'}
                   fill
-                  className="object-cover transition-transform duration-500 group-hover:scale-110"
+                  className="object-cover transition-transform duration-500 group-hover:scale-110 object-center"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent" />
                 <div className="absolute bottom-0 left-0 right-0 p-6 flex items-center gap-3">
