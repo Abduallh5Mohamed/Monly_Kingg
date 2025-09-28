0e54146921155b6a5a4f1257581e1b21f8818382
Abduallh Shadow <abduallhshadow@gmail.com>
2025-09-28 12:27:19 +0000

    فالورانت الكلمه اتاكلت اكتر العكس ياغبي

--- src/components/account-carousel.tsx
+++ src/components/account-carousel.tsx
@@ -21,7 +21,7 @@ const trendingGames: TrendingGame[] = [
   {
     name: '',
     logo: '/assets/valorant.jpg',
-    objectPosition: 'bottom',
+    objectPosition: 'center',
   },
   {
     name: '',
