cc1b5f2a4b88db52cafbfa9338630c2ce563dbd9
Abduallh5Mohamed <abduallh5mohamed@gmail.com>
2026-03-15 17:36:55 +0200

    refactor(page): remove FeaturedGamesSection from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from '@/components/sections/hero-section';
 import { WhyChooseUsSection } from '@/components/sections/why-choose-us-section';
-import { FeaturedGamesSection } from '@/components/sections/featured-games-section';
 import { SupportSection } from '@/components/sections/support-section';
 import { Footer } from '@/components/layout/footer';
 import Snowfall from '@/components/layout/snowfall';
@@ -11,7 +10,6 @@ export default function Home() {
       <Snowfall />
       <HeroSection />
       <WhyChooseUsSection />
-      <FeaturedGamesSection />
       <SupportSection />
       <Footer />
     </>
